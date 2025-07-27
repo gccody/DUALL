eee2bdd902309a6d464d3d88675334f1244302d5
Grant <gccody2010@live.com>
2025-07-27 14:10:31 -0400

    chore: Proper auth screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
+import ErrorView from "@/components/ErrorView";
+import LoadingView from "@/components/LoadingView";
 import { SettingsProvider } from "@/context/SettingsContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import * as LocalAuthentication from 'expo-local-authentication';
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
@@ -17,11 +18,12 @@ export default function RootLayout() {
     authenticate();
   }, []);
 
-  if (authenticated === false)
-    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>You are not authenticated!!!</Text></View>
-
   if (authenticated === null) {
-    return <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}><Text>Please Authenticate!!!</Text></View>
+    return <LoadingView />;
+  }
+
+  if (authenticated === false) {
+    return <ErrorView message={"You are not authenticated!!!"} />
   }
 
   return (
